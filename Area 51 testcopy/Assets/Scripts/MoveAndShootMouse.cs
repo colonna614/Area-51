8c1c2242bb58658d8b7ad0bd83cb9190dc05568c
colonna614 <colonna614@gmail.com>
2020-09-01 23:15:11 -0400

    v1.1.8 integrated AR functionality, started animation

--- Area 51 testcopy/Assets/Scripts/MoveAndShootMouse.cs
+++ Area 51 testcopy/Assets/Scripts/MoveAndShootMouse.cs
@@ -7,6 +7,8 @@ public class MoveAndShootMouse : MonoBehaviour
   public Transform firePoint;
   public Transform firePoint1;
   public Transform firePoint2;
+    public Transform firePointAR1;
+    public Transform firePointAR2;
 
     public static bool pistolState = true;
   public static bool isShotgunState = false;
@@ -29,8 +31,9 @@ public class MoveAndShootMouse : MonoBehaviour
       lookAngle = Mathf.Atan2(bulletPos.y, bulletPos.x) * Mathf.Rad2Deg;
       transform.rotation = Quaternion.Euler(0f, 0f, lookAngle);
 
-      if (AmmoCount.ammo > 0 && canShoot ==true){
-        bulletPos = transform.position;
+        if (AmmoCount.ammo > 0 && canShoot == true)
+        {
+            bulletPos = transform.position;
 
             if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space)) && isARState == false)
             {
@@ -40,28 +43,27 @@ public class MoveAndShootMouse : MonoBehaviour
                     ShootBullet();
                     AmmoCount.ammo -= 1;
                 }
-                else if (isKnifeState == true)
-                {
-                    stabSomeone();
-                }
                 else if (isShotgunState == true && AmmoCount.ammo >= 3)
                 {
                     SoundManagerScript.PlaySound("SFX/Shotgun");
                     ShootBulletSpread();
                     AmmoCount.ammo -= 3;
                 }
-               
-            }
-            else if ((Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Space)) && isARState == true)
-            {
-                SoundManagerScript.PlaySound("SFX/Pistol");
-                
-                ShootBullet();
-                //AmmoCount.ammo -= 1;
-            }
-        
 
-      }
+            }
+            else if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space)) && isARState == true)
+            {
+                InvokeRepeating("ShootBulletAR", 0f, .2f);
+            }
+            else if ((Input.GetKeyUp(KeyCode.Mouse0) || Input.GetKeyUp(KeyCode.Space)) && isARState == true)
+            {
+                CancelInvoke();
+            }
+        }
+        else if (AmmoCount.ammo == 0)
+        {
+            CancelInvoke();
+        }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             pistolState = true;
@@ -109,9 +111,30 @@ public class MoveAndShootMouse : MonoBehaviour
        firedBullet2.GetComponent<Rigidbody2D>().velocity = firePoint1.right;
        firedBullet3.GetComponent<Rigidbody2D>().velocity = firePoint2.right;
      }
-    public void stabSomeone()
+    public void ShootBulletAR()
     {
-        
+        float randFirePoint = Random.Range(1f, 4f);
+        if (randFirePoint < 2)
+        {
+            SoundManagerScript.PlaySound("SFX/Pistol");
+            AmmoCount.ammo -= 1;
+            GameObject firedBullet = Instantiate(bulletToRight, firePoint.position, firePoint.rotation);
+            firedBullet.GetComponent<Rigidbody2D>().velocity = firePoint.right;
+        }
+        else if (randFirePoint < 3)
+        {
+            SoundManagerScript.PlaySound("SFX/Pistol");
+            AmmoCount.ammo -= 1;
+            GameObject firedBulletAR1 = Instantiate(bulletToRight, firePointAR1.position, firePointAR1.rotation);
+            firedBulletAR1.GetComponent<Rigidbody2D>().velocity = firePointAR1.right;
+        }
+        else if (randFirePoint <= 4)
+        {
+            SoundManagerScript.PlaySound("SFX/Pistol");
+            AmmoCount.ammo -= 1;
+            GameObject firedBulletAR2 = Instantiate(bulletToRight, firePointAR2.position, firePointAR2.rotation);
+            firedBulletAR2.GetComponent<Rigidbody2D>().velocity = firePointAR2.right;
+        }
     }
 
 
